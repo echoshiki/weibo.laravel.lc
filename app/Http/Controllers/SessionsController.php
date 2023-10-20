b2ffee0e9ae7f926c962efbfddc91622373cc7e2
echoshiki <brokenworldand.me@gmail.com>
2023-10-20 15:10:12 +0800

    实现记住我功能

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -32,7 +32,7 @@ class SessionsController extends Controller
             'email' => 'required|email|max:255',
             'password' => 'required'
         ]);
-        if (Auth::attempt($credentials)) {
+        if (Auth::attempt($credentials, $request->has('remember'))) {
             # 验证通过
             session()->flash('success', '欢迎回来!');
             return redirect()->route('users.show', [Auth::user()]);
